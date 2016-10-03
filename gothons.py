c3a7039ddeda6c0e1d01160b6679fd5721d499d3
fredgoat <fredgoat@gmail.com>
2016-10-02 20:49:40 -0700

    Made the map and the basic engine

--- gothons.py
+++ gothons.py
@@ -42,55 +42,67 @@ super_seed = randint(1,1000)
 print "This seed is", super_seed
 seed(super_seed)    # this will let you go back to good randomnesses
 
-decay           = 0.8     # component branches die off by a power of this
-
-
-class Grid(object):
-    """The Grid is basically the screen or UI"""
-    def __init__(self, width=winwidth, height=winheight, character=' '):
-        self.grid = [[character for x in xrange(width)] for y in xrange(height)]
-
-    def ischar(self, coordinate, character=' '):
-        x, y = coordinate
-        line = self.grid[y]
-        if y < 0:                       # is this in the grid?
-            return False
-        if x < 0:
-            return False
-        if y >= len(self.grid):
-            return False
-        if x >= len(line):
-            return False
-        if line[x] == character:        # is it the thing?
-            return True
-        else:
-            return False
-
-stations.append(Station(outer_space, (0,0,'n'), {}))
-
 class Map(object):
 
     def __init__(self, start_scene):
-        pass
+        self.spine = [start_scene]
+        for x in xrange(randint(1,4)):
+            self.spine.append([randint(1, 5), randint(1, 5)])
+            self.spine[-1].insert(randint(0, 2), 0) # 0 is more corridor, 1-2 is deadend, 3-5 are side-scenes
+        the_bridge = TheBridge()
+        self.spine.append(the_bridge)
+        while True:
+            segment = randint(1,-2)
+            direction = randint(0,2)
+            if self.spine[segment][direction] == 0:
+                pass
+            else:
+                self.spine[segment][direction] = 6  # 6 will be the armory
+                break
+        while True:
+            segment = randint(1,-2)
+            direction = randint(0,2)
+            if self.spine[segment][direction] == 0 or 6:
+                pass
+            else:
+                self.spine[segment][direction] = 7  # 7 will be the escape pods
 
     def next_scene(self, scene_name):
         pass
 
     def opening_scene(self):
-        pass
+        self.spine[0].enter()
 
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.map = scene_map
+        self.location = (0,0) # 0 to whatever segment, 0-4 direction.  0 = main, 1 = left, 2 = forward, 3 = right
 
     def play(self):
-        print "welcome to Gothons from Planet Percal #25!  You're trapped on a Merchant Cruiser which has been overrun" \
-              "by Gothon storm troopers.  They hate you and want to eat you.  Your only hope lies in finding the" \
-              "Nukanuke in the Armory, setting it to detonate on the Bridge, and then finding an Escape Pod.  Good luck!"
-        the_map.opening_scene()
+        print "welcome to Gothons from Planet Percal #25!  You're trapped on a Merchant Cruiser which has been overrun " \
+              "by Gothon storm troopers.  They hate you and want to eat you.  Your only hope lies in finding the " \
+              "Nukanuke in the Armory, setting it to detonate on the Bridge, and then finding an Escape Pod."
+        self.map.opening_scene()
+
+    def forward(self):
+        self.location = (self.location[0]+1,0)
+
+    def backward(self):
+        self.location = (self.location[0]-1,0)
+
+    def left_turn(self):
+        self.location = (self.location[0],1)
+
+    def straight_turn(self):
+        self.location = (self.location[0],2)
+
+    def right_turn(self):
+        self.location = (self.location[0],3)
 
 class Scene(object):
+    def __init__(self):
+        self.items = []
 
     def enter(self):
         pass
@@ -99,20 +111,47 @@ class Death(Scene):
 
     def enter(self):
         while True:
-            print "You've died aboard the ship!  Your body is quickly devoured by a Gothon and since Earth is destroyed" \
+            print "You've died aboard the ship!  Your body is quickly devoured by a Gothon and since Earth is destroyed " \
             "mere minutes later, no one remembers you.  Do you want to try again?"
             retry = raw_input(prompt)
-            if retry == "yes" or "Yes" or "YES" or "y" or "N":
-                return True
-            elif retry == "no" or "No" or "NO" or "n" or "N":
-                return False
+            if retry.lower() in ("yes", "y"):
+                print "Well, then restart or whatever."
+                break
+            elif retry.lower() in ("no", "n"):
+                print "Okay, bye."
+                break
             else:
                 print "Sorry, what was that?"
 
 class CentralCorridor(Scene):
+    def __init__(self):
+        self.items = ["a Gothon soldier"]
 
     def enter(self):
-        pass
+        print "You find yourself at the end of a relatively wide central corridor with many twists and branches, " \
+        "running the length of the ship."
+        if "a Gothon soldier" in self.items:
+            print "A Gothon soldier, rounding the corner of the corridor, freezes for a second as it spots you.  What do you do?"
+            response = raw_input(prompt)
+            if response.lower() in ("punch", "punch it", "punch soldier", "punch gothon"):
+                print "You punch it and it dies!"
+                self.items.remove("a Gothon soldier")
+                self.enter()
+            else:
+                print "What are you even trying to do?  It shoots, eats, and leaves.  You're dead now, and inside a Gothon."
+                death = Death()
+                death.enter()
+        else:
+            for x in self.items:
+                print "You see %s here." % (x)
+            print "There's only one way to go, here.  Are you ready to go forward?"
+            response = raw_input(prompt)
+            if response.lower() in ("forward", "go forward", "y", "yes"):
+                the_game.forward()
+            else:
+                Print "Fine whatever, you die of starvation."
+                death = Death()
+                death.enter()
 
 class LaserWeaponArmory(Scene):
 
@@ -129,21 +168,9 @@ class EscapePod(Scene):
     def enter(self):
         pass
 
-
-class Map(object):
-
-    def __init__(self, start_scene):
-        pass
-
-    def next_scene(self, scene_name):
-        pass
-
-    def opening_scene(self):
-        pass
-
-
-the_map = Map('central_corridor')
-the_game = Engine(a_map)
+central_corridor = CentralCorridor()
+the_map = Map(central_corridor)
+the_game = Engine(the_map)
 the_game.play()
 
-# Do: make map._init_ build a map, starting with start_scene
+# Do: What happens when you go forward from the corridor?
